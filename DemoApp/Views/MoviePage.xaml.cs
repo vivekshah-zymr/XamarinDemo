ba242c992219ab2cc1538e6edc83f6ac75146522
vivek.shah-zymr <vivek.shah@zymr.com>
2017-08-02 14:50:38 +0530

    add library for Android media framwork, and update and update the android version with Android 7.0

--- DemoApp/Views/MoviePage.xaml.cs
+++ DemoApp/Views/MoviePage.xaml.cs
@@ -14,19 +14,19 @@ namespace DemoApp.Views
 
         void didTapSlider(object sender, EventArgs e)
         {
-			int i = 1;
+            int i = 1;
 
-			while (Navigation.NavigationStack.Count > 0 && Navigation.NavigationStack.Count >= i)
-			{
+            while (Navigation.NavigationStack.Count > 0 && Navigation.NavigationStack.Count >= i)
+            {
                 var stackPage = Navigation.NavigationStack[i - 1];
-				System.Diagnostics.Debug.WriteLine("=======: " + stackPage);
-                if(stackPage.GetType() == typeof(HomePage))
+                System.Diagnostics.Debug.WriteLine("=======: " + stackPage);
+                if (stackPage.GetType() == typeof(HomePage))
                 {
                     ((HomePage)stackPage).IsPresented = true;
                     break;
                 }
-				i++;
-			}
+                i++;
+            }
         }
 
         void didTapSearch(object sender, EventArgs e)
--- DemoApp/Views/SliderPage.xaml.cs
+++ DemoApp/Views/SliderPage.xaml.cs
@@ -14,25 +14,34 @@ namespace DemoApp.Views
 
         void didTapLogout(object sender, EventArgs e)
         {
-			System.Diagnostics.Debug.WriteLine("=====NavigationStack===== " + Navigation.NavigationStack.Count);
-			int i = 1;
-			while (Navigation.NavigationStack.Count > 0 && Navigation.NavigationStack.Count >= i)
-			{
-				System.Diagnostics.Debug.WriteLine("=======: " + Navigation.NavigationStack[i - 1]);
-				i++;
-			}
+			//System.Diagnostics.Debug.WriteLine("=====NavigationStack===== " + Navigation.NavigationStack.Count);
+			//int i = 1;
+			//while (Navigation.NavigationStack.Count > 0 && Navigation.NavigationStack.Count >= i)
+			//{
+			//	System.Diagnostics.Debug.WriteLine("=======: " + Navigation.NavigationStack[i - 1]);
+			//	i++;
+			//}
 
-			System.Diagnostics.Debug.WriteLine("=====ModalStack===== " + Navigation.ModalStack.Count);
-			int j = 1;
+			//System.Diagnostics.Debug.WriteLine("=====ModalStack===== " + Navigation.ModalStack.Count);
+			//int j = 1;
 
-			while (Navigation.ModalStack.Count > 1 && Navigation.ModalStack.Count >= i)
-			{
-				System.Diagnostics.Debug.WriteLine("=======: " + Navigation.ModalStack[i - 1]);
-				j++;
-			}
+			//while (Navigation.ModalStack.Count > 1 && Navigation.ModalStack.Count >= i)
+			//{
+			//	System.Diagnostics.Debug.WriteLine("=======: " + Navigation.ModalStack[i - 1]);
+			//	j++;
+			//}
+
 			//Navigation.RemovePage(this);
 
 			Application.Current.MainPage = new NavigationPage(new StartPage());
         }
+
+		private async void didTapOpenCamera(object sender, EventArgs e)
+		{
+			var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() { });
+
+			if (photo != null)
+				imgViewProfile.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+		}
     }
 }
